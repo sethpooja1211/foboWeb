5950aec66d3311fd47f31ef08e481d5235d16122
sethpooja1211 <poojaseth92@gmail.com>
2017-10-04 23:23:05 +0530

    remove some unuesd lines

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-(function($) { 
-      $('.smoothscroll').on('click', function (e) {                      
+(function($) {
+      $('.smoothscroll').on('click', function (e) {
            e.preventDefault();
            var target = this.hash,
            $target = $(target);
@@ -13,8 +13,8 @@
            $('html, body').stop(true, true).animate({
                   'scrollTop': offset
             }, 800, 'swing');
-      }); 
-      var header_logo = $("#main_content .logo-container");
+      });
+      var header_logo = $("#main_content .bg-dark");
       function isElementInView(element, fullyInView) {
       var pageTop = $(window).scrollTop();
       var pageBottom = pageTop + $(window).height();
@@ -33,34 +33,31 @@
             //topBar = $('header');
             topBar = $('nav');
          if (y > 670) {
-            topBar.addClass('fixed-top');
+            //topBar.addClass('fixed-top');
             header_logo.removeClass('bg-dark');
             header_logo.fadeIn(0.3,'slow').addClass('bg_trans');
-            navWrap.css("display", "initial");
-            header_logo.css({
+            //navWrap.css("display", "initial");
+            /*header_logo.css({
                 WebkitTransition : 'opacity 0.5s ease-in-out',
                 MozTransition    : 'opacity 0.5s ease-in-out',
                 MsTransition     : 'opacity 0.5s ease-in-out',
                 OTransition      : 'opacity 0.5s ease-in-out',
                 transition       : 'opacity 0.5s ease-in-out',
 
-            });
+            });*/
 
          }
         else {
-           navWrap.addClass('logo-container');
-           header_logo.removeClass('bg_trans');
+            //navWrap.addClass('logo-container');
+            header_logo.removeClass('bg_trans');
             header_logo.addClass('bg-dark');
-            header_logo.css({
+            /*header_logo.css({
                   WebkitTransition : 'opacity 0.3s ease-in-out',
                   MozTransition    : 'opacity 0.3s ease-in-out',
                   MsTransition     : 'opacity 0.3s ease-in-out',
                   OTransition      : 'opacity 0.3s ease-in-out',
                   transition       : 'opacity 0.3s ease-in-out'
-                  }); 
+                }); */
         }
   });
 })(jQuery);
-
-  
-  
